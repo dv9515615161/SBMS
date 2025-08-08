22c5cba62d319d2e71931f1539cc8f16685a7190
vijay <dv9515615161@gmail.com>
2025-08-08 16:12:11 +0530

    Complete Mini Project Layered Application

--- Spring-Boot-MiniProject/src/main/java/com/nit/SpringBootMiniProjectApplication.java
+++ Spring-Boot-MiniProject/src/main/java/com/nit/SpringBootMiniProjectApplication.java
@@ -1,14 +1,35 @@
 package com.nit;
 
+import com.nit.controller.PayRollOperationController;
+import com.nit.model.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootMiniProjectApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx= SpringApplication.run(SpringBootMiniProjectApplication.class, args);
+        //get ioc container
+		ConfigurableApplicationContext ctx= SpringApplication.run(SpringBootMiniProjectApplication.class, args);
+        //get controller class object
+        PayRollOperationController controller= ctx.getBean("payroll",PayRollOperationController.class);
+        //invoke the business method
+        try {
+            List< Employee> list=controller.showAllEmployeesByDesignation("CLERK","MANAGER","SALESMAN");
+            //process the result
+            list.forEach(employee -> {System.out.println(employee);});
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        //close ioc container
+        ctx.close();
 
 	}
 
---
+++ Spring-Boot-MiniProject/src/main/java/com/nit/controller/PayRollOperationController.java
@@ -0,0 +1,20 @@
+package com.nit.controller;
+
+import com.nit.model.Employee;
+import com.nit.service.IEmployeeMgmtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller("payroll")
+public class PayRollOperationController {
+    @Autowired
+    private IEmployeeMgmtService service;
+    public List<Employee> showAllEmployeesByDesignation(String desg1,String desg2,String desg3) throws Exception{
+        //use service
+         List<Employee> list=service.fetchEmployeeByDesignation(desg1, desg2, desg3);
+        return list;
+    }
+
+}
